18059db86d4f71cd53bc1be5b3770b0be7df01c1
SrZafkiell <daniel.rayo@correounivalle.edu.co>
2022-11-18 00:42:06 -0500

    Initial commit

---
+++ Ejercicio 00.py
@@ -0,0 +1,16 @@
+# Author: Daniel Alejandro Ceron Rayo
+# Software development student at Universidad del Valle Cali-Colombia / 1st semester
+# Professor: Giovanny Hidalgo / FUNDAMENTOS DE PROGRAMACION IMPERATIVA
+
+'''
+Ejercicio  / Exercise 
+Spanish: Desarrollar un programa que permita calcular el área de un triángulo dados su base y su altura.
+English: Make a program to calc de area of a triangle with a given base and height
+'''
+
+numero1 = int(input())
+numero2 = int(input())
+
+resultado = (numero1*numero2)/2
+
+print("El área del triángulo es:",float(resultado))
---
+++ Warmup 1 to 4.py
@@ -0,0 +1,50 @@
+# Author: Daniel Alejandro Ceron Rayo
+# Software development student at Universidad del Valle Cali-Colombia / 1st semester
+# Professor: Giovanny Hidalgo / FUNDAMENTOS DE PROGRAMACION IMPERATIVA
+
+'''
+Ejercicio 1 / Exercise 1
+Spanish: Usando Python imprima su primer mensaje "Hola Mundo"
+English: Using Python, print your first message "Hola Mundo"  (Hello World)
+'''
+
+print("hola Mundo")
+
+'''
+Ejercicio  / Exercise 
+Spanish: Realice un programa para sumar dos números
+English: Make a program to add two numbers
+'''
+
+numero1 = int(input())
+numero2 = int(input())
+
+resultado = numero1 + numero2
+
+print("La suma es:",int(resultado))
+
+'''
+Ejercicio  / Exercise 
+Spanish: Realice un programa para restar dos números
+English: Make a program to subtract two numbers
+'''
+
+numero1 = float(input())
+numero2 = float(input())
+
+resultado = numero1 - numero2
+
+print("La resta es:",resultado)
+
+'''
+Ejercicio  / Exercise 
+Spanish: Realice un programa para dividir dos números
+English: Make a program to divide two numbers
+'''
+
+numero1 = int(input())
+numero2 = int(input())
+
+resultado = numero1/numero2
+
+print("La respuesta es:",int(resultado))
